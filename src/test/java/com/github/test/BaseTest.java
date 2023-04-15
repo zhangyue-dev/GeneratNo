b38c2895f813df349b6978eaa7beb64a2354d879
zhangyue <abstime@163.com>
2023-04-15 18:26:35 +0800

    测试并发获取id

--- src/test/java/com/github/test/BaseTest.java
+++ src/test/java/com/github/test/BaseTest.java
@@ -10,6 +10,16 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.StopWatch;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 @SpringBootTest(classes = LeafdemoApplication.class)
 @RunWith(SpringRunner.class)
 public class BaseTest {
@@ -28,5 +38,23 @@ public class BaseTest {
         }
         stopWatch.stop();
         System.err.println(String.format("init process ms: %s", stopWatch.getTotalTimeMillis()));
+    }
+
+    /**
+     * 并发获取id
+     *
+     * @see org.apache.catalina.connector.Connector
+     */
+    @Test
+    public void currentIdInit() {
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(
+                200, 200,
+                0L, TimeUnit.MICROSECONDS,
+                new LinkedBlockingDeque<>(50000)
+        );
+        CompletableFuture[] arrayFuture = IntStream.range(0, 50000).mapToObj(i -> CompletableFuture.runAsync(() -> {
+            System.err.println(segmentService.getId("test_tag").toString());
+        }, executor)).toArray(CompletableFuture[]::new);
+        CompletableFuture.allOf(arrayFuture).join();
     }
 }
